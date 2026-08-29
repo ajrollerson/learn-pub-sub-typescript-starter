8e5649d8cddbc8b08f034d8caf56b4d43d350de9
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-08-29 12:33:28 +0700

    Add interactive REPL to server for pause and resume commands

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,8 +1,9 @@
 import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { clientWelcome, getInput } from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
+import { printServerHelp } from "../internal/gamelogic/gamelogic.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -14,6 +15,37 @@ async function main() {
   try {
     const username = await clientWelcome();
     await declareAndBind(conn, ExchangePerilDirect, `${PauseKey}.${username}`, PauseKey, SimpleQueueType.Transient);
+    printServerHelp()
+
+    while (true) {
+      const input = await getInput()
+
+      if (input.length === 0) {
+        continue;
+      }
+      const command = input[0];
+      if (command === "pause") {
+        try {
+          console.log("Pausing!");
+            await publishJSON(confirmChannel, ExchangePerilDirect, PauseKey, { isPaused: true });
+          } catch (err) {
+            console.log(err);
+          }
+      } else if (command === "resume") {
+        try {
+            console.log("Resuming!");
+            await publishJSON(confirmChannel, ExchangePerilDirect, PauseKey, { isPaused: false });
+          } catch (err) {
+            console.log(err);
+          }
+      } else if (command === "quit") {
+        console.log("Thanks for playing! Exiting...")
+        break;
+      } else {
+        console.log("Sorry, I don't understand. Please write again.")
+      }
+    }
+
   } catch (err) {
     console.log(err);
   }
