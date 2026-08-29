0d239e99a5a0cce70e39f89c4547a3d1decaef47
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-08-29 17:46:27 +0700

    Add JSON message publisher and move handler for client

--- src/client/handlers.ts
+++ src/client/handlers.ts
@@ -1,10 +1,19 @@
+import type { ArmyMove } from "../internal/gamelogic/gamedata.js";
 import type { GameState } from "../internal/gamelogic/gamestate.js";
 import type { PlayingState } from "../internal/gamelogic/gamestate.js";
+import { handleMove, MoveOutcome } from "../internal/gamelogic/move.js";
 import { handlePause } from "../internal/gamelogic/pause.js";
 
 export function handlerPause(gs: GameState): (ps: PlayingState) => void {
     return function (ps: PlayingState): void {
         handlePause(gs, ps);
-        console.log("> ");
+        process.stdout.write("> ");
+    };
+}
+
+export function handlerMove(gs: GameState): (move: ArmyMove) => void {
+    return function (move: ArmyMove): void {
+        handleMove(gs, move);
+        process.stdout.write("> ");
     };
 }
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,11 +1,12 @@
 import amqp from "amqplib";
-import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { ArmyMovesPrefix, ExchangePerilDirect, ExchangePerilTopic, PauseKey } from "../internal/routing/routing.js";
 import { clientWelcome, commandStatus, getInput, printClientHelp, printQuit } from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind, SimpleQueueType, subscribeJSON } from "../internal/pubsub/consume.js";
 import { GameState } from "../internal/gamelogic/gamestate.js";
 import { commandSpawn } from "../internal/gamelogic/spawn.js";
 import { commandMove } from "../internal/gamelogic/move.js";
-import { handlerPause } from "./handlers.js";
+import { handlerMove, handlerPause } from "./handlers.js";
+import { publishJSON } from "../internal/pubsub/publish.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -17,7 +18,9 @@ async function main() {
     const username = await clientWelcome();
     await declareAndBind(conn, ExchangePerilDirect, `${PauseKey}.${username}`, PauseKey, SimpleQueueType.Transient);
     const gameState = new GameState(username);
-    subscribeJSON(conn, ExchangePerilDirect, `${PauseKey}.${username}`, PauseKey, SimpleQueueType.Transient, handlerPause(gameState));
+    const newChannel = await conn.createConfirmChannel()
+    await subscribeJSON(conn, ExchangePerilDirect, `${PauseKey}.${username}`, PauseKey, SimpleQueueType.Transient, handlerPause(gameState));
+    await subscribeJSON(conn, ExchangePerilTopic, `${ArmyMovesPrefix}.${username}`, `${ArmyMovesPrefix}.*`, SimpleQueueType.Transient, handlerMove(gameState));
 
     process.on("SIGINT", async () => {
     console.log("Programme shutting down!");
@@ -45,7 +48,8 @@ async function main() {
           }
       } else if (command === "move") {
         try {
-            commandMove(gameState, input);
+            const move = commandMove(gameState, input);
+            await publishJSON(newChannel, ExchangePerilTopic, `${ArmyMovesPrefix}.${username}`, move)
           } catch (err) {
             if (err instanceof Error) {
               console.error(err.message);
